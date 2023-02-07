9754d853976f84a6be511e36d070e8da5b626cf3
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-08 00:25:57 +0300

    required eklendi

--- myapp/src/Form.js
+++ myapp/src/Form.js
@@ -5,19 +5,19 @@ export default function Form({datasaver, submit}) {
 
     return (
         <form onSubmit={submit}>
-            
+
             <label>Isim:
-            <input type="text" name="isim" onChange={datasaver}/>
+            <input type="text" name="isim" onChange={datasaver} required/>
             </label>
             
 
             <label>E-mail:
-            <input type="text" name="mail" onChange={datasaver}/>
+            <input type="text" name="mail" onChange={datasaver} required/>
             </label>
             
 
             <label>Rol:
-            <input type="text" name="rol" onChange={datasaver}/>
+            <input type="text" name="rol" onChange={datasaver} required/>
             </label>
             
             <button type="submit">KAYDET</button>
